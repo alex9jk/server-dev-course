e3f4276507786ab916d66ac014937e5c8d49fd87
Alex Kramer <alex9jk@gmail.com>
2019-02-14 11:22:28 -0500

    Revert "init hw3"
    This reverts commit 36b682ae26a2e524535cf4b1a28737b90e2b4f05.
    
    	deleted:    hw3/kramer_hw4.zip

--- hw3/hw4_4.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once("h4_3.php");
-require_once("hw4_1.php");
-
-$darren = new BritishPerson("Darren", "Wogman");
-$darren ->set_height_weight(150,80);
-
-echo "<p>Hello " . $darren->get_full_name() . "... your bmi is " . $darren ->calculate_bmi();
-?>
